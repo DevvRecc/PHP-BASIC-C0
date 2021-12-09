459197dee12dd4ee98a8a302cf88c6f90247e638
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-09 10:21:07 +0100

    3.1 afgemaakt

---
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+
+$myMovies = ["Spiderman", "Home-Alone", "Star-Wars"];
+
+echo $myMovies[0];
+echo "<br>";
+echo $myMovies[1];
+echo "<br>";
+echo $myMovies[2];
+
+?>
+    
+</body>
+</html>
