a2c95056e7065930bf52d738fc118bda1674feaa
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-09 10:29:11 +0100

    3.2 afgemaakt

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+
+$myAnimals = ["Lion", "Tiger", "Dog"];
+$myAnimals [0] = "Tiger";
+$myAnimals [1] = "Dog";
+$myAnimals [2] = "Lion";
+
+
+echo $myAnimals [0];
+echo "<br>";
+echo $myAnimals [1];
+echo "<br>";
+echo $myAnimals [2];
+
+
+?>
+    
+</body>
+</html>
