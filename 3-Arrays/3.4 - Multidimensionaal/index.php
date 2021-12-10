c6aca2a06b9cc36166332ae190878b3d4bf6867e
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-10 13:14:01 +0100

    3.4 afgemaakt

---
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<?php 
+
+$abcGetallen = [ ["a","b","c"], [24,56,78,90] ];
+
+echo $abcGetallen [0][0];
+echo $abcGetallen [1][1] . "<br>";
+echo $abcGetallen [0][2];
+echo $abcGetallen [1][3]; 
+
+?>
+
+</body>
+</html>
