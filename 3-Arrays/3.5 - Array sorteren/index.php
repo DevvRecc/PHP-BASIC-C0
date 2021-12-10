f1106b037b6f1a8e8009e349a87e9cdf988ba162
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-10 13:32:30 +0100

    3.5 afgemaakt

---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<?php 
+
+$laag_hoog = [676, 5345, 78657, 3, 46, 21 ,47];
+    asort($laag_hoog);
+
+$hoog_laag = [676, 5345, 78657, 3, 46, 21 ,47];
+    arsort($hoog_laag);
+
+    print_r($laag_hoog);
+    print_r($hoog_laag);
+
+
+?>
+
+</body>
+</html>
