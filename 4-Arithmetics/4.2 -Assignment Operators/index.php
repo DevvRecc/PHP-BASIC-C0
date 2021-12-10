7ed694abd3ba9d871e7abc6c04ed0a9d3e21b96c
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-10 13:49:44 +0100

    4.2 afgemaakt

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+
+$score = 20;
+
+$score += 10;
+echo $score . "<br>";
+
+$score -= 10;
+echo $score . "<br>";
+
+$score *= 10;
+echo $score . "<br>";
+
+$score /= 10;
+echo $score . "<br>";
+
+?>
+    
+</body>
+</html>
