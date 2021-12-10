67e5566400d715ceb7dd3b13ade3923b512206a6
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-10 13:40:36 +0100

    4.1 gemaakt

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<?php 
+
+$getal1 = 2;
+$getal2 = 4;
+
+echo $getal1 + $getal2 . "<br>";
+echo $getal1 - $getal2 . "<br>";
+echo $getal1 * $getal2 . "<br>";
+echo $getal1 / $getal2;
+
+?>
+
+</body>
+</html>
