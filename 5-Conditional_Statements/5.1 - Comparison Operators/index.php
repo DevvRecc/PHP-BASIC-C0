9b8744b1cb401bd75023f23dff48d345bc39d6e2
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-13 11:03:11 +0100

    5.1 gemaakt

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<?php 
+
+$test1 = 12;
+$test2 = "gewoon tekst";
+
+var_dump($test1 == $test2) . "<br>";
+var_dump($test1 === $test2) . "<br>";
+var_dump($test1 != $test2) . "<br>";
+var_dump($test1 > $test2) . "<br>";
+var_dump($test1 < $test2) . "<br>";
+var_dump($test1 <> $test2) . "<br>";
+var_dump($test1 >= $test2) . "<br>";
+var_dump($test1 <= $test2);
+
+?>
+
+</body>
+</html>
