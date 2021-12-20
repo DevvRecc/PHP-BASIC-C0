4dccb1757db409ea7bf70ec6781a2fb9c08c7401
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-20 12:39:56 +0100

    5.6 afgemaakt!

--- 5-Conditional_Statements/5.6 - Logical Operators/index.php
+++ 5-Conditional_Statements/5.6 - Logical Operators/index.php
@@ -4,7 +4,20 @@
     
 <?php 
 
+$var1 = 4;
+$var2 = 6;
 
+if($var1 > 5 || $var1 < 5 ) {
+    echo "Het getal is kleiner dan 5!" . "<br>";
+} else {
+    echo "Het getal is groter dan 5!" . "<br>";
+}
+
+if($var2 < 5 || $var2 > 5 ) {
+    echo "Het getal is groter dan 5!" . "<br>";
+} else {
+    echo "Het getal is kleiner dan 5" . "<br>";
+}
 
 ?>
 
