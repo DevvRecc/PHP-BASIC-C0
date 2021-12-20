f48898335f6b6b55df806dece9a2b609c846d15e
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-20 13:01:32 +0100

    6.2 afgemaakt!

--- 6-Functions/6.2 - strlen()/index.php
+++ 6-Functions/6.2 - strlen()/index.php
@@ -4,6 +4,16 @@
 
 <?php 
 
+function mijnNaamIsGroot(){
+    $voorNaam = "Recep";
+    $achterNaam = "Yildiz";
+    
+    $geheleNaam = $voorNaam . " " . $achterNaam;
+    $geheleNaam = strtoupper("Mijn naam is $geheleNaam");
+    echo $geheleNaam;
+}
+mijnNaamIsGroot();
+
 ?>
     
 </body>
